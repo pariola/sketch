e411c68a29d92252d097ec45bd5d12ef495e5cef
Blessing Pariola <blessingpariola@zoho.com>
2021-10-22 03:01:09 +0100

    added helper tool inBounds

--- pkg/canvas/tools.go
+++ pkg/canvas/tools.go
@@ -1,5 +1,10 @@
 package canvas
 
+// inBounds checks if position (x,y) is within bounds
+func inBounds(x, y, boundX, boundY int) bool {
+	return !(x < 0 || x >= boundX || y < 0 || y >= boundY)
+}
+
 // inXBoundary checks if position (x,y) is on the X-axis boundary of the rectangle
 func inXBoundary(x, y int, r Rectangle) bool {
 	return x >= r.posX && x < r.posX+r.width && (y == r.posY || y == r.posY+r.height-1)
--- pkg/canvas/tools_test.go
+++ pkg/canvas/tools_test.go
@@ -6,6 +6,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_inBounds(t *testing.T) {
+
+	type boundTest struct {
+		x, y   int
+		result bool
+	}
+
+	// check a 2x2 matrix
+	tests := []boundTest{
+		{0, 0, true},
+		{0, 1, true},
+		{0, 2, false},
+		{0, 3, false},
+		{1, 0, true},
+		{1, 1, true},
+		{1, 2, false},
+		{1, 3, false},
+		{2, 0, false},
+		{2, 1, false},
+		{2, 2, false},
+		{2, 3, false},
+		{3, 0, false},
+		{3, 1, false},
+		{3, 2, false},
+		{3, 3, false},
+	}
+
+	for _, b := range tests {
+		result := inBounds(b.x, b.y, 2, 2)
+		assert.Equalf(t, b.result, result, "cell [%d, %d]", b.x, b.y)
+	}
+
+}
+
 func Test_inXBoundary(t *testing.T) {
 
 	type boundary struct {
